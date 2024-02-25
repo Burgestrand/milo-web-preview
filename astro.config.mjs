e50a5ed86f7e63f3263b2c0692a3b3603685819a
Kim Burgestrand <kim@burgestrand.se>
2024-02-25 23:15:47 +0100

    Deploy to pages with Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  site: 'https://burgestrand.github.io',
+  base: '/milo-web-preview',
 });
