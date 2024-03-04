bde7b3b90b809938ca5c7c77afe010604f3aa733
Kim Burgestrand <kim@burgestrand.se>
2024-03-04 17:03:27 +0100

    Add and configure tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), tailwind()],
   site: 'https://burgestrand.github.io',
   base: '/milo-web-preview/',
 });
