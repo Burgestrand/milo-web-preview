a564fa662a242a028d7233223faec2089bb542a6
Kim Burgestrand <kim@burgestrand.se>
2024-03-04 22:37:37 +0100

    Add shadows to match material UI of Milo site

--- src/components/Settings/Form.tsx
+++ src/components/Settings/Form.tsx
@@ -22,11 +22,11 @@ export default function Form() {
   }, [])
 
   return (
-    <form className="flex flex-row flex-wrap place-content-center border-t-white py-2 px-4 gap-2">
+    <form className="flex flex-row flex-wrap place-content-center py-2 px-4 gap-2">
       {Object.entries($colors).map(([name, filament]) => (
         <label key={name} className="flex flex-col place-items-center">
           <span className="text-white font-semibold lowercase" style={{ fontVariantCaps: "small-caps" }}>{titles[name]}</span>
-          <select name={name} value={filament.id} onChange={handleColorChange} className="text-xs rounded-md">
+          <select name={name} value={filament.id} onChange={handleColorChange} className="text-xs rounded-md text-black">
             {filaments.map(({ id, name }) =>
               <option key={id} value={id}>{name}</option>
             )}
