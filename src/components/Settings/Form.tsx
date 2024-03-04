c1306851871c19011b798efeee7e9c9314faca3a
Kim Burgestrand <kim@burgestrand.se>
2024-03-04 18:35:01 +0100

    Replace all styling with TailwindCSS and update design a bit.

--- src/components/Settings/Form.tsx
+++ src/components/Settings/Form.tsx
@@ -5,6 +5,14 @@ import { colors as colorsStore, setColor } from '@stores/settings'
 import type { ID as FilamentID } from '@config/filaments'
 import { filaments, findById } from '@config/filaments'
 
+const titles: Record<Color, string> = {
+  primary: "Primary",
+  secondary: "Secondary",
+  tertiary: "Tertiary",
+  accent: "Accent",
+  tpu: "TPU",
+}
+
 export default function Form() {
   const $colors = useStore(colorsStore)
 
@@ -13,12 +21,16 @@ export default function Form() {
     setColor(event.target.name as Color, filament)
   }, [])
 
+  const style = {
+    gridAutoColumns: 'minmax(0, 1fr)',
+  }
+
   return (
-    <form id="settings-form">
+    <form className="flex flex-row border-t-white py-2 px-4 gap-1">
       {Object.entries($colors).map(([name, filament]) => (
-        <label key={name}>
-          <span>{name} ({filament.id})</span>
-          <select name={name} value={filament.id} onChange={handleColorChange}>
+        <label key={name} style={style} className="flex flex-col place-items-center">
+          <span className="">{titles[name]}</span>
+          <select name={name} value={filament.id} onChange={handleColorChange} className="text-xs rounded-md">
             {filaments.map(({ id, name }) =>
               <option key={id} value={id}>{name}</option>
             )}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'shark': {
+          '50': '#f6f7f9',
+          '100': '#eceef2',
+          '200': '#d5dae2',
+          '300': '#b0b9c9',
+          '400': '#8593ab',
+          '500': '#667691',
+          '600': '#515e78',
+          '700': '#434c61',
+          '800': '#3a4252',
+          '900': '#343a46',
+          '950': '#1e2129',
+        },
+        accent: '#EC6565',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
 };
